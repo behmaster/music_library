78e081d8db5d3a083fe884666287192d44554fcf
Robbie Behm <behmaster@gmail.com>
2022-07-12 20:48:14 -0500

    react-router-dom

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useContext } from "react";
+import { SearchContext } from "../context/SearchContext";
 
-function SearchBar(props){
-    // We can comment out our searchTerm state variable as we are not using it!
-    // let [searchTerm, setSearchTerm] = useState('')
-const [event, setEvent] = useState(null)
-    useEffect(() => {
-        const timeOutId = setTimeout(() => props.handleSearch(event, event.target.value), 500);
-        return () => clearTimeout(timeOutId);
-      }, [event]);
-    
+function SearchBar(props) {
+  // We can comment out our searchTerm state variable as we are not using it!
+  // let [searchTerm, setSearchTerm] = useState('')
+  const { term, handleSearch } = useContext(SearchContext);
+  // const [event, setEvent] = useState(null)
+  //     useEffect(() => {
+  //         const timeOutId = setTimeout(() => props.handleSearch(event, event.target.value), 500);
+  //         return () => clearTimeout(timeOutId);
+  //       }, [event]);
 
-    return (
-            <form>
-                <input type="text" placeholder="Search Here"
-                    onChange={
-                        (e) => setEvent(e)
-                    } />
-                <input type="submit" />
-            </form>
-    )
+  return (
+    <form>
+      <input ref={term} type="text" placeholder="Search Here" />
+      <button onClick={(e) => handleSearch(e, term.current.value)}>
+        Submit
+      </button>
+    </form>
+  );
 }
 
-export default SearchBar
-
-
-
+export default SearchBar;
