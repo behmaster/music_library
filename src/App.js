3f3143bf1714cf97bbead95fd5ee21753315aa56
Robbie Behm <behmaster@gmail.com>
2022-07-18 20:48:15 -0500

    Combined branch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-import { useEffect, useState, useRef, Fragment } from "react";
+import { useEffect, useState, useRef, Fragment, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
-import { DataContext } from "./context/DataContext";
-import { SearchContext } from "./context/SearchContext";
 import AlbumView from "./components/AlbumView";
 import ArtistView from "./components/ArtistView";
+import Spinner from "./components/Spinner";
+import { DataContext } from "./context/DataContext";
+import { SearchContext } from "./context/SearchContext";
+import { createResource as fetchData } from './helper'
 
 const App = () => {
-  let [search, setSearch] = useState("");
+  let [searchTerm, setSearchTerm] = useState("");
   let [message, setMessage] = useState("Search for Music!");
-  let [data, setData] = useState([]);
+  let [data, setData] = useState(null);
   let searchInput = useRef("");
 
-  const API_URL = "https://itunes.apple.com/search?term=";
+  const API_URL = `https://itunes.apple.com/search?term=`;
 
+//   useEffect(() => {
+//     if (searchTerm) {
+//       document.title=`${searchTerm} Music`
+//       console.log(fetchData(searchTerm))
+//       setData(fetchData(searchTerm))
+//   }
+//   }, [searchTerm])
+
+//   const handleSearch = (e, searchTerm) => {
+//     e.preventDefault()
+//     setSearchTerm(searchTerm)
+//   }
+
+// //Solution code2
   const handleSearch = (e, term) => {
-    e.preventDefault();
-    const fetchData = async () => {
-      document.title = `${term} Music`;
-      const response = await fetch(API_URL + term);
-      const resData = await response.json();
-      if (resData.results.length > 0) {
-        return setData(resData.results);
-      } else {
-        return setMessage("Not Found.");
-      }
-    };
-    fetchData();
-  };
+    e.preventDefault()
+    setData(fetchData(term))
+  }
+
+//ORIGINAL CODE === this code works
+//   const handleSearch = (e, term, path) => {
+//     e.preventDefault();
+//     const fetchData = async () => {
+//       document.title = `${term} Music`;
+//       const response = await fetch(API_URL + term);
+//       const resData = await response.json();
+//       if (resData.results.length > 0) {
+//         return setData(resData.results);
+//       } else {
+//         return setMessage("Not Found.");
+//       }
+//     };
+//     fetchData();
+//   };
+
+const renderGallery = () => {
+    if(data){
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      )
+    }
+  }
 
   return (
     <div>
@@ -48,7 +80,7 @@ const App = () => {
                   <SearchBar />
                 </SearchContext.Provider>
                 <DataContext.Provider value={data}>
-                  <Gallery />
+                  {renderGallery()}
                 </DataContext.Provider>
               </Fragment>
             }
